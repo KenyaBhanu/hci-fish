baaa8d10d9c458f852c34d816b8f46eb50c4edc2
KenyaBhanu <kenyabhanuwatipranoto_1313621019@mhs.unj.ac.id>
2022-11-02 09:19:12 +0700

    Edited tab 1 and 2

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,29 +1,173 @@
 <template>
   <ion-page>
-    <ion-header>
+    <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Home</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
-    </ion-content>
+    <ion-header collapse="condense">
+       <ion-toolbar>
+        <ion-title size="large">Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <div>
+      <ion-row>
+        <ion-col>
+          <ion-card class="white-cards">
+            <ion-card-header class="card-head">
+             Kolam
+            </ion-card-header>
+
+            <ion-card-content class="card-content">
+              <ion-card-title class="card-title">
+                10
+              </ion-card-title>
+              <ion-card-subtitle class="card-sub">
+
+              </ion-card-subtitle>
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col>
+          <ion-card class="white-cards">
+            <ion-card-header class="card-head">
+             Kolam Aktif
+            </ion-card-header>
+
+            <ion-card-content class="card-content">
+              <ion-card-title class="card-title">
+                8
+              </ion-card-title>
+              <ion-card-subtitle class="card-sub">
+
+              </ion-card-subtitle>
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+      </ion-row>
+
+<!-- second row -->
+      <ion-row>
+        <ion-col>
+          <ion-card class="white-cards">
+            <ion-card-header class="card-head">
+             Ikan Hidup
+            </ion-card-header>
+
+            <ion-card-content class="card-content">
+              <ion-card-title class="card-title">
+                182
+              </ion-card-title>
+              <ion-card-subtitle class="card-sub">
+                ekor
+              </ion-card-subtitle>
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col>
+          <ion-card class="white-cards">
+            <ion-card-header class="card-head">
+             Ikan Mati
+            </ion-card-header>
+
+            <ion-card-content class="card-content">
+              <ion-card-title class="card-title">
+                89
+              </ion-card-title>
+              <ion-card-subtitle class="card-sub">
+                ekor
+              </ion-card-subtitle>
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+      </ion-row>
+
+<!-- third row -->
+      <ion-row>
+        <ion-col>
+          <ion-card class="white-cards">
+            <ion-card-header class="card-head">
+             Panen 2022
+            </ion-card-header>
+
+            <ion-card-content class="card-content">
+              <ion-card-title class="card-title">
+                109
+              </ion-card-title>
+              <ion-card-subtitle class="card-sub">
+                ekor
+              </ion-card-subtitle>
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col>
+          <ion-card class="white-cards">
+            <ion-card-header class="card-head">
+             Total Pakan
+            </ion-card-header>
+
+            <ion-card-content class="card-content">
+              <ion-card-title class="card-title">
+                223
+              </ion-card-title>
+              <ion-card-subtitle class="card-sub">
+                ekor
+              </ion-card-subtitle>
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+      </ion-row>
+    </div>
+
+      <!-- slider-->
+    <ion-title>TOTAL BERAT IKAN</ion-title>
+      <div class="slider">
+        <ion-row>
+          <ion-col>
+            <ion-card class="slider-cards">
+              <ion-card-header class="card-head">
+                Ikan
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </div>
+      <div class="slider">
+        <ion-row>
+          <ion-col>
+            <ion-card class="slider-cards">
+              <ion-card-header class="card-head">
+                Ikan
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </div>
+      <!-- slider pH-->
+    <ion-title>KUALITAS AIR</ion-title>
+    <div class="slider">
+        <ion-row>
+          <ion-col>
+            <ion-card class="slider-cards">
+              <ion-card-title class="card-title">
+                pH
+              </ion-card-title>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </div>
+      </ion-content>
+
   </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default  defineComponent({
   name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
 });
 </script>
--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,17 +2,111 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Kolam</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
+          <ion-title size="large">Kolam</ion-title>
         </ion-toolbar>
       </ion-header>
-      
-      <ExploreContainer name="Tab 2 page" />
+    <ion-row>
+      <ion-card class="kolam-card">
+        <ion-card-header class="kolam-card-head">
+          Alpha
+          <button ion-button block class="active" style="align:center">Aktif</button>
+        </ion-card-header>
+        
+        <ion-card-content class="kolam-card-content">
+          <ion-card-title class="kolam-card-title">
+            01-09-2022 <br>
+            90 hari <br>
+            200 ekor
+          </ion-card-title>
+          <ion-card-subtitle class="kolam-card-sub">
+
+          </ion-card-subtitle>
+        </ion-card-content>
+      </ion-card>
+    </ion-row>
+    <ion-row>
+      <ion-card class="kolam-card">
+        <ion-card-header class="kolam-card-head">
+          Beta
+          <button ion-button block class="inactive" style="align:center">Tidak Aktif</button>
+        </ion-card-header>
+        
+        <ion-card-content class="kolam-card-content">
+          <ion-card-title class="kolam-card-title">
+            01-09-2022 <br>
+            90 hari <br>
+            200 ekor
+          </ion-card-title>
+          <ion-card-subtitle class="kolam-card-sub">
+
+          </ion-card-subtitle>
+        </ion-card-content>
+      </ion-card>
+    </ion-row>
+    <ion-row>
+      <ion-card class="kolam-card">
+        <ion-card-header class="kolam-card-head">
+          Charlie
+          <button ion-button block class="active" style="align:center">Aktif</button>
+        </ion-card-header>
+        
+        <ion-card-content class="kolam-card-content">
+          <ion-card-title class="kolam-card-title">
+            01-09-2022 <br>
+            90 hari <br>
+            200 ekor
+          </ion-card-title>
+          <ion-card-subtitle class="kolam-card-sub">
+
+          </ion-card-subtitle>
+        </ion-card-content>
+      </ion-card>
+    </ion-row>
+    <ion-row>
+      <ion-card class="kolam-card">
+        <ion-card-header class="kolam-card-head">
+          Delta
+          <button ion-button block class="active" style="align:center">Aktif</button>
+        </ion-card-header>
+        
+        <ion-card-content class="kolam-card-content">
+          <ion-card-title class="kolam-card-title">
+            01-09-2022 <br>
+            90 hari <br>
+            200 ekor
+          </ion-card-title>
+          <ion-card-subtitle class="kolam-card-sub">
+
+          </ion-card-subtitle>
+        </ion-card-content>
+      </ion-card>
+    </ion-row>
+    <ion-row>
+      <ion-card class="kolam-card">
+        <ion-card-header class="kolam-card-head">
+          Epsilon
+          <button ion-button block class="active" style="align:center">Aktif</button>
+        </ion-card-header>
+        
+        <ion-card-content class="kolam-card-content">
+          <ion-card-title class="kolam-card-title">
+            01-09-2022 <br>
+            90 hari <br>
+            200 ekor
+          </ion-card-title>
+          <ion-card-subtitle class="kolam-card-sub">
+
+          </ion-card-subtitle>
+        </ion-card-content>
+      </ion-card>
+    </ion-row>
+
     </ion-content>
   </ion-page>
 </template>
@@ -20,10 +114,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default defineComponent({
   name: 'Tab2Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
 });
 </script>
